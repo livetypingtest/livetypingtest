30a9d83079048ce59e4ba3f29fa0cd54636c3093
Sadiq53 <jafarussadiq098@gmail.com>
2024-11-21 17:30:56 +0530

    addind data

--- typing test front/src/util/API_URL.js
+++ typing test front/src/util/API_URL.js
@@ -1,16 +1,16 @@
 // const ADMIN_API_URL = "http://localhost:8080/admin";
-const ADMIN_API_URL = "https://typing-test-server-z0yx.onrender.com/admin";
+// const ADMIN_API_URL = "https://typing-test-server-z0yx.onrender.com/admin";
 // const ADMIN_API_URL = "http://54.147.134.149:8080/admin";
-// const ADMIN_API_URL = "https://api.livetypingtest.com:8080/admin";
+const ADMIN_API_URL = "https://api.livetypingtest.com:8080/admin";
 
 // const USER_API_URL = "http://localhost:8080/user";
-const USER_API_URL = "https://typing-test-server-z0yx.onrender.com/user";
+// const USER_API_URL = "https://typing-test-server-z0yx.onrender.com/user";
 // const USER_API_URL = "http://54.147.134.149:8080/user";
-// const USER_API_URL = "https://api.livetypingtest.com:8080/user";
+const USER_API_URL = "https://api.livetypingtest.com:8080/user";
 
 // const BASE_API_URL = "http://localhost:8080";
-const BASE_API_URL = "https://typing-test-server-z0yx.onrender.com";
+// const BASE_API_URL = "https://typing-test-server-z0yx.onrender.com";
 // const BASE_API_URL = "http://54.147.134.149:8080";
-// const BASE_API_URL = "https://api.livetypingtest.com:8080";
+const BASE_API_URL = "https://api.livetypingtest.com:8080";
 
 export { ADMIN_API_URL, USER_API_URL, BASE_API_URL };
