4af6950d7d6e1f8b7d56ec127a15a74a2d0ea8fc
Sadiq53 <jafarussadiq098@gmail.com>
2024-11-30 20:57:26 +0530

    added sitemap

--- typing test front/src/modules/RootModule.jsx
+++ typing test front/src/modules/RootModule.jsx
@@ -3,8 +3,6 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Outlet } from 'react-router-dom'
 import { handleLocalDataCalling } from '../redux/UserDataSlice';
 import { handleGetAboutData, handleGetPrivacyData, handleGetTermData } from '../redux/DynamicPagesDataSlice';
-import useDynamicTitle from '../component/shared/dynamicTitle/useDynamicTitle';
-import updateSitemap from '../../public/script/updateSitemap';
 
 const RootModule = () => {
 
@@ -19,18 +17,6 @@ const RootModule = () => {
     // useEffect(()=>{dispatch(handleGetPrivacyData())}, [])
     useEffect(()=>{dispatch(handleGetTermData())}, [])
 
-    const blogData = useSelector(state => state.UserDataSlice.blog)
-    let urls = []
-
-    useEffect(()=>{
-        blogData.map(value => urls.push(`https://livetypingtest.com/blog/${value.permalink}`))
-    }, [blogData])
-
-    useEffect(()=>{
-        if(urls?.length !== 0) {
-            updateSitemap(urls)
-        }
-    }, [urls])
     
     return (
         <Outlet />
--- typing test front/src/modules/UserModule.jsx
+++ typing test front/src/modules/UserModule.jsx
@@ -11,8 +11,6 @@ import { dynamicToast } from '../component/shared/Toast/DynamicToast';
 import { handleGetAboutData, handleGetPrivacyData, handleGetTermData } from '../redux/DynamicPagesDataSlice';
 import useDynamicTitle from '../component/shared/dynamicTitle/useDynamicTitle';
 import { notificationToast } from '../component/shared/Toast/NotificationToats';
-import MetaUpdater from '../util/MetaUpdater';
-import updateSitemap from '../../public/script/updateSitemap';
 
 
 const UserModule = () => {
@@ -150,20 +148,6 @@ const UserModule = () => {
     useEffect(()=>{dispatch(handleGetAboutData())}, [])
     // useEffect(()=>{dispatch(handleGetPrivacyData())}, [])
     useEffect(()=>{dispatch(handleGetTermData())}, [])
-
-
-    const blogData = useSelector(state => state.UserDataSlice.blog)
-    let urls = []
-
-    useEffect(()=>{
-        blogData.map(value => urls.push(`https://livetypingtest.com/blog/${value.permalink}`))
-    }, [blogData])
-
-    useEffect(()=>{
-        if(urls?.length !== 0) {
-            updateSitemap(urls)
-        }
-    }, [urls])
 
     return (
         <>
