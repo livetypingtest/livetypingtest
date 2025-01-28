b142b656c165f0900045460b7c97c41e418c40d8
Sadiq53 <jafarussadiq098@gmail.com>
2025-01-28 16:49:35 +0530

    description changes of seo

--- typing test front/src/component/shared/helmet/DynamicTitle.jsx
+++ typing test front/src/component/shared/helmet/DynamicTitle.jsx
@@ -6,10 +6,10 @@ const DynamicTitle = ({ title, description, icon }) => {
             {/* Set the page title */}
             <title>{title || 'Live Typing Test - English Typing Speed Test & Practice in 60 sec'}</title>
             {/* Meta description for SEO */}
-            <meta name="description" content={description || 'Live Typing Test - English Typing Speed Test & Practice in 60 sec'} />
+            <meta name="description" content={description || 'Free Online English Typing Speed Test & Practice to increase average words per minute (WPM) in 1 minute, 3 minutes and 5 minutes with Certificate.'} />
             {/* Open Graph tags for social media */}
             <meta property="og:title" content={title || 'Live Typing Test - English Typing Speed Test & Practice in 60 sec'} />
-            <meta property="og:description" content={description || 'Live Typing Test - English Typing Speed Test & Practice in 60 sec'} />
+            <meta property="og:description" content={description || 'Free Online English Typing Speed Test & Practice to increase average words per minute (WPM) in 1 minute, 3 minutes and 5 minutes with Certificate.'} />
             <meta property="og:image" content={icon || '/assets/images/favicon.png'} />
             <link rel="icon" href={icon || '/assets/images/favicon.png'} />
         </Helmet>
